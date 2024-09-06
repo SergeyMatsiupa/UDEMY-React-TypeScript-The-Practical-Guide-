6b07d0b0ea5e79631842cbf3d8ec54f94ca4c6d2
Sergey Matsiupa <msvcontact@gmail.com>
2024-09-06 12:33:03 +0200

    (after 56; not passing props to Container)

--- adv-components/src/App.tsx
+++ adv-components/src/App.tsx
@@ -1,4 +1,5 @@
 import Button from "./components/Button";
+import Container from "./components/Container";
 import Input from "./components/Input";
 
 function App() {
@@ -8,11 +9,14 @@ function App() {
       <Input id="name" label="Your name" disabled/>
       <Input id="age" label="Your age" type="number" />
       <p>
-        <Button el="button">A Button</Button>
+        {/* <Button el="button">A Button</Button> */}
+        <Button >A Button</Button>
       </p>
       <p>
-        <Button el="anchor" href="https://google.com">A Link</Button>
+        {/* <Button el="anchor" href="https://google.com">A Link</Button> */}
+        <Button href="https://google.com">A Link</Button>
       </p>
+      <Container as={Button} onClick={() => {}} style={{color: 'red'}}>Button!</Container>
     </>
   );
 }
